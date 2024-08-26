d2b18d71b51895312d97ff9b4ffa12a24cb830ee
Shishir Singha <164762673+shishirsingha1@users.noreply.github.com>
2024-08-26 08:47:46 -0700

    hokkolti harailam

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registation form</title>
+    <link rel="stylesheet" href="Assets/css/bootstrap.min.css">
+
+</head>
+<body>
+    home
+<script src="Assets/js/bootstrap.min.js"></script>
+</body>
+</html>
